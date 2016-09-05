56635e61a72e6eaed00f5e916b1be2862b5c3509
Nicholas Zuber <zuber.nicholas@gmail.com>
2016-09-04 21:51:06 -0400

    lknasdknsdlkn i need sleep

--- src/bloom_filter.hpp
+++ src/bloom_filter.hpp
@@ -2,19 +2,26 @@
 #ifndef INCLUDE_BLOOM_FILTER_HPP
 #define INCLUDE_BLOOM_FILTER_HPP
 
-class BloomFilter {
-public:
-    BloomFilter () {};
+class BloomFilter
+{
+
+    public:
     
-    bool test ();
+        BloomFilter () {};
     
-    void add ();
+        template<typename T>
+        inline bool contains (const T& t) const;
     
-    virtual ~BloomFilter() {
-        // destructor
-    };
+        template<typename T>
+        inline void add (const T& t) const;
     
-private:
+        inline void clear();
+    
+        virtual ~BloomFilter() {};
+        
+    private:
+        // hash functions, remember we only need 2 to simulate k
+        // mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm i think custom bitarray so we can make it light
 };
 
-#endif
+#endif /* INCLUDE_BLOOM_FILTER_HPP */
