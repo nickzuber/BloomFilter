d9d8ea4008f9bdcb76243ae916d8bf7298f1caac
Nicholas Zuber <zuber.nicholas@gmail.com>
2016-09-04 21:16:51 -0400

    some more init files and structuring

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,11 +3,11 @@
 //  BloomFilter
 //
 //  Created by Nicolas Zuber on 9/3/16.
-//  Copyright © 2016 Nicolas Zuber. All rights reserved.
+//  Copyright © 2016 Nicholas Zuber. All rights reserved.
 //
 
 #include <iostream>
-#include "src/bloom_filter.hpp"
+#include "../src/bloom_filter.hpp"
 using namespace std;
 
 int main(int argc, const char * argv[]) {
